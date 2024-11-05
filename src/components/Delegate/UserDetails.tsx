b41700520a55fd86fecbd9be3f8fca24c95e8c52
abbasnosrati <abbas.nosrati68@gmail.com>
2024-11-05 08:30:21 +0330

    edit text

--- src/components/Delegate/UserDetails.tsx
+++ src/components/Delegate/UserDetails.tsx
@@ -22,7 +22,7 @@ export const UserDetails = () => {
       <div className="flex flex-col gap-5 sm:gap-5 sm:flex-row sm:h-[170px] h-auto mt-5 sm:mt-0">
         <div className="w-full text-xs md:text-sm  transition-all text-white action-sidebar rounded-2xl  flex flex-col justify-center gap-3 px-3 py-3 md:rounded-2xl  bg-so-dark-gray md:px-6 md:py-4">
           <div className="flex justify-between">
-            <p>STAKED AMOUNT</p>
+            <p>Staked Amount</p>
             <div className="font-semibold sm:text-lg text-md">
               {userDelegateBalances
                 ? `${userDelegateBalances?.dsp} PION`
@@ -30,7 +30,7 @@ export const UserDetails = () => {
             </div>
           </div>
           <div className="flex justify-between">
-            <div>REWARD</div>
+            <div>Reward</div>
             <div className="font-semibold text-md sm:text-lg">1.215</div>
           </div>
         </div>
