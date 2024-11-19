4c713ce2565452014d9cd7efaf67b15cfe86c7d3
abbasnosrati <abbas.nosrati68@gmail.com>
2024-11-19 16:12:20 +0330

    set bnb mainnet chain

--- src/web3/chains.ts
+++ src/web3/chains.ts
@@ -1,13 +1,14 @@
-import { bscTestnet } from "wagmi/chains";
-// import { bsc } from "wagmi/chains";
 import { Chain } from "wagmi/chains";
 
-export const supportedChains: Chain[] = [bscTestnet];
-// export const supportedChains: Chain[] = [bsc];
+// import { bscTestnet } from "wagmi/chains";
+import { bsc } from "wagmi/chains";
+
+// export const supportedChains: Chain[] = [bscTestnet];
+export const supportedChains: Chain[] = [bsc];
 
 export enum SupportedChainId {
-  chainId = bscTestnet.id,
-  // chainId = bsc.id,
+  // chainId = bscTestnet.id,
+  chainId = bsc.id,
 }
 
 export const SUPPORTED_CHAIN_IDS: SupportedChainId[] = Object.values(
